1b8f0a734777acea681f6ee3c58a3a06a8047d14
Fauzi Kliman <fauzimerfoo@gmail.com>
2020-02-25 16:39:58 -0800

    Simplify userToken passing for redux actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,5 +2,5 @@ import { SET_USER_TOKEN } from "./actionTypes";
 
 export const setUserToken = userToken => ({
     type: SET_USER_TOKEN,
-    payload: { userToken }
+    userToken
 });
--- src/redux/reducers/userToken.js
+++ src/redux/reducers/userToken.js
@@ -3,7 +3,7 @@ import { SET_USER_TOKEN } from "../actionTypes";
 export default function(state = null, action) {
     switch (action.type) {
         case SET_USER_TOKEN:
-            return action.payload.userToken;
+            return action.userToken;
         default:
             return state;
     }
