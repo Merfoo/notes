7af7d18113aaa9d69cb5ce80a7f9d66b2300079a
Fauzi Kliman <fauzimerfoo@gmail.com>
2020-03-01 14:40:14 -0800

    Load/Save state to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,20 @@
 import { createStore } from "redux";
 import rootReducers from "./reducers";
 
-export default createStore(rootReducers);
+import { loadState, saveState } from "./localStorage";
+
+const persistedState = loadState();
+
+const store = createStore(
+    rootReducers,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+    saveState({
+        userToken: store.getState().userToken
+    });
+})
+
+export default store;
